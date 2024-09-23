9cd50dc3f9d6881fc313cc14534829b7a12d1a94
Genesissongag <genesissongag@gmail.com>
2024-09-23 09:42:53 +0800

    Create activity2.js

---
+++ activity2.js
@@ -0,0 +1,24 @@
+let firstFavNum = 5;
+// Create Variable name (firstFavNum) & store your favorite number. 
+let secondFavNum = 20;
+// Add (firstFavNum & secondFavNumber). 
+let sum = firstFavNum + secondFavNum;
+// Subtract (firstFavNum & secondFavNumber). 
+let difference = firstFavNum- secondFavNum;
+// Multiply (firstFavNum & secondFavNumber).
+let product = firstFavNum * secondFavNum;
+// Divided (firstFavNum & secondFavNumber).
+let quotient = firstFavNum / secondFavNum; 
+// Check (firstFavNum Mod secondFavNumber).
+let remainder = firstFavNum % secondFavNum;
+// Check the power of (firstFavNum) raise to secondFavNun. 
+let power = Math.pow(firstFavNum , secondFavNum);
+// Finally log all variables to the console using code runner in vscode.
+console.log(firstFavNum);
+console.log(secondFavNum);
+console.log(sum);
+console.log(difference);
+console.log(product);
+console.log(quotient);
+console.log(remainder);
+console.log(power);

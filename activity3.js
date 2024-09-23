7eddb8d677b90ca2b84b148d26323b885dcec77d
Genesissongag <genesissongag@gmail.com>
2024-09-23 09:43:35 +0800

    Create activity3.js

---
+++ activity3.js
@@ -0,0 +1,13 @@
+// Create a variable to store whether JavaScript is a programming language
+let isJsProgrammingLanguage = true;
+// Create a variable to store whether JavaScript is hard
+let isJsHard = false;
+// Add favNumber with undefined, which will result in NaN
+// Create a variable to store my favorite number
+let favNumber = 05;
+let result = favNumber + undefined;
+// Log all variables to the console
+console.log("isJsProgrammingLanguage: ", isJsProgrammingLanguage);
+console.log("isJsHard: ", isJsHard);
+console.log("favNumber: ", favNumber);
+console.log("Result: ", result);

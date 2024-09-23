18dcbe0ba98a4c02c5ef1fab77acda09b089952c
Genesissongag <genesissongag@gmail.com>
2024-09-23 09:44:14 +0800

    Create activity4.js

---
+++ activity4.js
@@ -0,0 +1,32 @@
+// Create a variable to store my first favorite number
+let firstFavNumb = 5;
+// Create a variable to store my second favorite number
+let secondFavNumb = 20;
+// Check if firstFavNumb is greater than secondFavNumb
+let isFirstGreater = firstFavNumb > secondFavNumb;
+// Check if firstFavNumb is less than secondFavNumb
+let isFirstLess = firstFavNumb < secondFavNumb;
+// Check if firstFavNumb is greater than or equal to secondFavNumb
+let isFirstGreaterOrEqual = firstFavNumb >= secondFavNumb;
+// Check if firstFavNumb is less than or equal to secondFavNumb
+let isFirstLessOrEqual = firstFavNumb <= secondFavNumb;
+// Check if firstFavNumb is equal to secondFavNumb using strict equality operator
+let isFirstStrictEqual = firstFavNumb === secondFavNumb;
+// Check if firstFavNumb is equal to secondFavNumb using loose equality operator
+let isFirstLooseEqual = firstFavNumb == secondFavNumb;
+// Check if firstFavNumb is not equal to secondFavNumb using strict non-equality operator
+let isFirstStrictNotEqual = firstFavNumb !== secondFavNumb;
+// Check if firstFavNumb is not equal to secondFavNumb using loose non-equality operator
+let isFirstLooseNotEqual = firstFavNumb != secondFavNumb;
+
+// Log all variables to the console
+console.log(`firstFavNumb: ${firstFavNumb}`);
+console.log(`secondFavNumb: ${secondFavNumb}`);
+console.log(`isFirstGreater: ${isFirstGreater}`);
+console.log(`isFirstLess: ${isFirstLess}`);
+console.log(`isFirstGreaterOrEqual: ${isFirstGreaterOrEqual}`);
+console.log(`isFirstLessOrEqual: ${isFirstLessOrEqual}`);
+console.log(`isFirstStrictEqual: ${isFirstStrictEqual}`);
+console.log(`isFirstLooseEqual: ${isFirstLooseEqual}`);
+console.log(`isFirstStrictNotEqual: ${isFirstStrictNotEqual}`);
+console.log(`isFirstLooseNotEqual: ${isFirstLooseNotEqual}`);

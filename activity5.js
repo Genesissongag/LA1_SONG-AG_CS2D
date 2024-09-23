f16e5ec0f6060bed21f8aafbd96e88d4057f0fb2
Genesissongag <genesissongag@gmail.com>
2024-09-23 09:44:51 +0800

    Create activity5.js

---
+++ activity5.js
@@ -0,0 +1,19 @@
+// Create a variable to store my favorite actor's first name
+let favActorFirstName = "kyung-soo";
+// Create a variable to store my favorite actor's last name
+let favActorLastName = " Do ";
+// Create a variable to store the full name of my favorite actor
+let fullName = favActorFirstName + " " + favActorLastName;
+// Create a variable to store the uppercase version of the full name
+let uppercase = fullName.toUpperCase();
+// Create a variable to store a message about my favorite actor
+let message = "My favorite Actor Is ${Do Kyung-soo} and I love his acting.";
+// Append the message with the favorite show of the actor
+message += " His best show is 100 days my prince.";
+
+// Log all variables to the console
+console.log("favActorFirstName: ${kyung-soo}");
+console.log(favActorLastName);
+console.log(fullName);
+console.log(uppercase);
+console.log(message);

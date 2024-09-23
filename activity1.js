e9d3fcc7af1414ba73df33dab8de4738fe290d3a
Genesissongag <genesissongag@gmail.com>
2024-09-23 09:30:24 +0800

    Create activity1.js

---
+++ activity1.js
@@ -0,0 +1,10 @@
+let GenesisSongAg = ("Genesis Song-ag");  //Create variable name (Genesis Song-ag)
+let whatDoYouWannaBecomeInYourLife = ("programmer");  //Create variable name (whatDoYouWannaBecomeInYourLife) "programmer" 
+let gender = ("female");  //Create variable name (female
+let facebookAcct = ("Genesis Song-ag");  //Create variable name (Genesis Song-ag)
+
+//Finally log all variables to the console using code runner in vscode.
+console.log("Name: ", GenesisSongAg);
+console.log("WhatDoYouWannaBecomeInYourlife: ", whatDoYouWannaBecomeInYourLife);
+console.log("Gender: ", gender);
+console.log("Facebook Account: ", facebookAcct);
